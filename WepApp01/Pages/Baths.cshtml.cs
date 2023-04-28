a032d78ab11ef5d4a94c73b37e142c236eaf5e45
Kingsley Brodie <kingsley@ultimatetiles.com.au>
2023-04-28 16:36:45 +1000

    added bath.cshtml

---
+++ WepApp01/Pages/Baths.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace UltimateTilesProj.Pages
+{
+    public class BathsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
